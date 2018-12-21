a1f6af05512b84592ec5dca7c0dfaa4dad2fa820
Eric Rodriguez <eric.rodriguez@eniro.com>
2018-12-21 13:02:21 +0100

    FS2 version works better sync

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "kafkaZIOProducer"
+name := "KafkaFS2Producer"
 
 version := "0.1"
 
@@ -17,7 +17,7 @@ libraryDependencies += "org.typelevel" %% "cats-effect" % "1.1.0"
 
 libraryDependencies += "net.cakesolutions" %% "scala-kafka-client" % "2.0.0"
 
-mainClass in assembly := Some("com.eniro.KafkaProducer")
+mainClass in assembly := Some("com.eniro.KafkaFS2Producer")
 
 addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.7")
 addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.2.4")
--- src/main/scala/com/eniro/KafkaProducer.scala
+++ src/main/scala/com/eniro/KafkaProducer.scala
@@ -6,13 +6,15 @@ import fs2.{Stream, io, text}
 import java.nio.file.Paths
 import java.util.concurrent.Executors
 
-import cakesolutions.kafka.KafkaProducer
+import cakesolutions.kafka.{KafkaProducer, KafkaProducerRecord}
 import cakesolutions.kafka.KafkaProducer.Conf
+import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.common.serialization.StringSerializer
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
-object Converter extends IOApp {
+object KafkaFS2Producer extends IOApp {
   private val blockingExecutionContext =
     Resource.make(IO(ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))))(ec => IO(ec.shutdown()))
 
@@ -24,20 +26,38 @@ object Converter extends IOApp {
   val extractKey: String => String = (line: String) => {
     val start = line.indexOf(":")
     val end = line.indexOf(",")
-    line.substring(start + 2, end - 1)
+    val msg = Try(line.substring(start + 2, end - 1))
+    msg match {
+      case Success(m) => m
+      case Failure(_) => {
+        println(line)
+        ""
+      }
+    }
   }
 
-  val processMessages: Stream[IO, Unit] = (file: String) => Stream.resource(blockingExecutionContext).flatMap { blockingEC =>
+  val sendMessage: String => Future[RecordMetadata] = (data: String) => {
+    val record = KafkaProducerRecord("julio.genio.stream", Some(extractKey(data)), data)
+    producer.send(record)
+  }
 
+  val processMessages: String => Stream[IO, Unit] = (file: String) => Stream.resource(blockingExecutionContext).flatMap { blockingEC =>
     io.file.readAll[IO](Paths.get(file), blockingEC, 4096)
       .through(text.utf8Decode)
       .through(text.lines)
-      .map(line => fahrenheitToCelsius(line.toDouble).toString)
-      .intersperse("\n")
-      .through(text.utf8Encode)
-      .through(io.file.writeAll(Paths.get("testdata/celsius.txt"), blockingEC))
+      .map(line => sendMessage(line))
+      /*.mapAsyncUnordered(6)(line => IO(sendMessage(line).onComplete {
+        case Success(mdt) => ()
+        case Failure(err) => println(err)
+      }(blockingEC)))*/
   }
 
-  def run(args: List[String]): IO[ExitCode] =
-    processMessages.compile.drain.as(ExitCode.Success)
+  def run(args: List[String]): IO[ExitCode] = {
+    val filename = args(0)
+    val t0 = System.currentTimeMillis()
+    processMessages(filename).compile.drain.map(_ =>  {
+      val t1 = System.currentTimeMillis()
+      println("Elapsed time: " + (t1 - t0) + "ms")
+    }).as(ExitCode.Success)
+  }
 }
